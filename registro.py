3244d45f29d4641adf6c599ce5682745779df6c8
jose123456879 <totitosst@gmail.com>
2016-06-08 08:42:13 -0600

    registro
    se intenta crear(esta en proceso)

---
+++ registro.py
@@ -0,0 +1,14 @@
+lista_de_estuduantes=[ ]
+lista_notas=[ ]
+print("1. crear estuduante \n 2. ingrese nota \n 3. reporte de notas \n 4. salir")
+opc=(int(input("elija una opcion: "))
+if opc==1:
+	e=input("ingrese un nombre:" )
+	lista_de_estuduantes.append(e)
+if opc==2:
+	nota=int(input("ingrese una nota: "))
+	lista_notas.append(nota)
+if opc==3:
+	lista_de_estuduantes
+	lista_de_estuduantes[e]=lista_notas.appendnota[nota]
+	print(lista_de_estuduantes[e])

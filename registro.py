61d1574092a12d27272ef2dda62a1ac938c751fd
jose123456879 <totitosst@gmail.com>
2016-06-09 08:40:18 -0600

    quickfix
    se intenta terminar

--- registro.py
+++ registro.py
@@ -1,14 +1,21 @@
+oo="si"
 lista_de_estuduantes=[ ]
 lista_notas=[ ]
-print("1. crear estuduante \n 2. ingrese nota \n 3. reporte de notas \n 4. salir")
-opc=(int(input("elija una opcion: "))
-if opc==1:
-	e=input("ingrese un nombre:" )
-	lista_de_estuduantes.append(e)
-if opc==2:
-	nota=int(input("ingrese una nota: "))
-	lista_notas.append(nota)
-if opc==3:
-	lista_de_estuduantes
-	lista_de_estuduantes[e]=lista_notas.appendnota[nota]
-	print(lista_de_estuduantes[e])
+
+while oo=="si":
+	print("1. crear estuduante \n 2. ingrese nota \n 3. reporte de notas \n 4. salir")
+	opc=int(input("elija una opcion: "))
+	if opc==1:
+		e=input("ingrese un nombre:" )
+		lista_de_estuduantes.append(e)
+	if opc==2:
+		nota=int(input("ingrese una nota: "))
+		lista_notas.append(nota)
+	if opc==3:
+		notas_estudiante={
+		"Alumno":"lista_de_estuduantes[e]",
+		"notas":"lista_notas[nota]"
+		}
+		print(notas_estudiante)
+
+	oo=input("desea continuar?")
